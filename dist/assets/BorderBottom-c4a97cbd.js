4283a437440374048d17bd9e005f5bf47dc697da
anikushalam <rjsingh.ak@gmail.com>
2023-12-07 08:18:08 +0530

    06-12-2023 global

---
+++ dist/assets/BorderBottom-c4a97cbd.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-9e136078.js";import{s as r}from"./Utils.module-5cada88c.js";const m=({customStyle:o})=>t.jsx("div",{style:{...o},className:r.border_bottom});export{m as B};
