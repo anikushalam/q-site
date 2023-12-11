7908f51adef69dfc868924dc7ce6a2028864c740
anikushalam <rjsingh.ak@gmail.com>
2023-12-11 11:34:22 +0530

    11-12-2023 glpobal

---
+++ dist/assets/BorderBottom-a08b9e4c.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-f1249455.js";import{s as r}from"./Utils.module-5cada88c.js";const m=({customStyle:o})=>t.jsx("div",{style:{...o},className:r.border_bottom});export{m as B};
