3e565a5fc2b06ccbea6d35af42050f4415f9a52e
anikushalam <rjsingh.ak@gmail.com>
2024-02-24 00:21:06 +0530

    24-02-2024 global

--- dist/assets/BorderBottom-8e26885e.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-387e3eee.js";import{s as r}from"./Utils.module-5cada88c.js";const m=({customStyle:o})=>t.jsx("div",{style:{...o},className:r.border_bottom});export{m as B};
---
+++ dist/assets/InsHome-4a220a5b.js
@@ -0,0 +1,1 @@
+import{j as r,L as n}from"./index-b01623f7.js";function s(){return r.jsx(r.Fragment,{children:r.jsx(n,{})})}export{s as default};
